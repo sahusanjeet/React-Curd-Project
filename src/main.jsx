91adde3066c5a032601d6d8e076b435cae4ae239
sahu sanjeet <sahusanjeet1600@gmail.com>
2025-05-29 19:16:23 +0530

    Update main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -1,14 +1,12 @@
-import { createRoot } from 'react-dom/client'
-import App from './App.jsx'
-import "./Style.css";
-import { BrowserRouter } from 'react-router-dom'
+import { createRoot } from "react-dom/client";
+import App from "./App.jsx";
+import "./index.css";
+import { ToastContainer } from "react-toastify";
+import Wrapper from "./Wrapper.jsx";
 
 createRoot(document.getElementById("root")).render(
-    
-
-<BrowserRouter>
-<App/>
-</BrowserRouter> 
+    <Wrapper>
+        <App />
+        <ToastContainer position="top-center" />
+    </Wrapper>
 );
-
-
